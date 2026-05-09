9a9c6f701a4fe5ad87d5a09bd6429b73f50c38f5
Yeri Orlando <yeriorlando@gmail.com>
2026-05-09 18:02:02 -0400

    Klynn Start

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro({ preset: "vercel" }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
